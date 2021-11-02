4d76a59ad6d670d067bdcf3471b0dc30f623c7c3
Billy Zhang <billyzhang663@gmail.com>
2021-11-02 08:24:32 -0700

    build: NFT ready

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,7 +11,7 @@ async function main() {
   console.log("nftMarket deployed to:", nftMarket.address);
 
   
-  const NFT = await hre.ethers.getContractFactory("NFT");
+  const NFT = await hre.ethers.getContractFactory("TokenMinds");
   const nft = await NFT.deploy(nftMarket.address);
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
